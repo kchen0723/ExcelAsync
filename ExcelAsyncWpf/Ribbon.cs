6f83e035cb1332f60f9295151b53f35ae8652609
kchen0723 <kchen0723@users.noreply.github.com>
2016-05-03 09:56:53 -0700

    disable/enable ribbon button
    disable/enable ribbon button

--- ExcelAsyncWpf/Ribbon.cs
+++ ExcelAsyncWpf/Ribbon.cs
@@ -15,6 +15,8 @@ namespace ExcelAsyncWpf
     [ComVisible(true)]
     public class Ribbon : ExcelRibbon
     {
+        private IRibbonUI m_Ribbon;
+        private bool m_IsSignalEnabled;
         public override string GetCustomUI(string RibbonID)
         {
             XmlDocument doc = new XmlDocument();
@@ -46,6 +48,26 @@ namespace ExcelAsyncWpf
         public void btnClockClick(IRibbonControl control)
         {
             ExcelOperate.ExcelApp.AddContentMenu();
+        }
+
+        public void RibbonUI_OnLoad(IRibbonUI ribbonUI)
+        {
+            m_Ribbon = ribbonUI;
+            MessageBox.Show("Ribbon UI Loading");
+        }
+
+        public void btnEyeClick(IRibbonControl control)
+        {
+            if (this.m_Ribbon != null)
+            {
+                this.m_Ribbon.Invalidate();
+            }
+        }
+
+        public bool OnSignalEnabled(IRibbonControl control)
+        {
+            m_IsSignalEnabled = !m_IsSignalEnabled;
+            return m_IsSignalEnabled;
         }
     }
 }
