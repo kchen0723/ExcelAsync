31a500f9905b4f36aaf0ffa3cbcb6a56073ba48e
kchen0723 <kchen0723@gmail.com>
2016-05-18 16:13:49 -0700

    Add RangeManager.cs

---
+++ ExcelAsyncWpf/ExcelOperator/CustomPropertyManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+
+namespace ExcelAsyncWpf.ExcelOperator
+{
+    public class CustomPropertyManager
+    {
+        public static dynamic GetWorkBookCustomProperty(Workbook wk, string Name)
+        {
+            dynamic cps = wk.CustomDocumentProperties;
+            dynamic item = null;
+            dynamic result = null;
+            for (int i = 1; i <= cps.Count; i++) //VB start from 1
+            {
+                item = cps.Item(i);
+                if (string.Compare(item.Name, Name, true) == 0)
+                {
+                    result = item;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public static string GetWorkBookPropertyString(Workbook wk, string Name)
+        {
+            string result = string.Empty;
+            dynamic cp = GetWorkBookCustomProperty(wk, Name);
+            if (cp != null)
+            {
+                result = cp.Value;
+            }
+            return result;
+        }
+
+        public static void SetWorkBookProperty(Workbook wk, string propertyName, string propertyValue)
+        {
+            dynamic cp = GetWorkBookCustomProperty(wk, propertyName);
+            if (cp == null)
+            {
+                dynamic cps = wk.CustomDocumentProperties;
+                cps.Add(propertyName, false, MsoDocProperties.msoPropertyTypeString, propertyValue);
+            }
+            else
+            {
+                cp.Value = propertyValue;
+            }
+        }
+    }
+}
---
+++ ExcelAsyncWpf/ExcelOperator/RangeManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelAsyncWpf.ExcelOperator
+{
+    public static class RangeManager
+    {
+        public static Name GetName(Workbook wk, string rangeName)
+        {
+            try
+            {
+                return wk.Names.Item(rangeName);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Range GetNameRange(Name name)
+        {
+            try
+            {
+                return name.RefersToRange;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static ListObject GetListObject(Worksheet sheet, string rangeName)
+        {
+            try
+            {
+                return sheet.ListObjects[rangeName];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Range GetListObjectRange(ListObject listObject)
+        {
+            try
+            {
+                return listObject.Range;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
