1861c56fdb958bce633fcb489addf97f676ea88d
kchen0723 <kchen0723@gmail.com>
2016-06-16 15:54:50 -0700

    Add main thread logic named space

---
+++ ExcelAsync/MainThreadLogic/ReadWriteRange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelAsync.MainThreadLogic
+{
+    internal class ReadWriteRange
+    {
+        internal static Range WriteToRange(object[,] response, Range targetRange)
+        {
+            int rowsCount = response.GetLength(0);
+            int columnsCount = response.GetLength(1);
+            ExcelReference target = new ExcelReference(targetRange.Row - 1, targetRange.Row + rowsCount - 2, targetRange.Column - 1, targetRange.Column + columnsCount - 2);
+            target.SetValue(response);
+            return targetRange.Resize[rowsCount, columnsCount];
+        }
+
+        internal static object[,] ReadFromRange()
+        {
+            object[,] result = null;
+            ExcelReference selection = new ExcelReference(0, 4, 0, 2);
+            object selectContent = selection.GetValue();
+            if (selectContent is object[,])
+            {
+                result = selectContent as object[,];
+            }
+            else if (selectContent is double)
+            {
+                result = new object[,] { { selectContent } };
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExcelAsync/MainThreadLogic/readme.txt
@@ -0,0 +1,5 @@
+﻿All excel operation should be written in this namespace. 
+All classes in this namespace should be internal or private.
+All internal method should call CheckIsExcelUIMainThread() method to make sure it run on Excel Main thread.
+
+If a call is from context menu, formula, or ribbon, then it is on main thread.
