79c4e52982f5938f456f2602762cbc991d21fa19
kchen0723 <kchen0723@gmail.com>
2016-06-29 11:17:15 -0700

    Close window

--- ExcelWvvm/ViewModel/GoogleHistoryViewModel.cs
+++ ExcelWvvm/ViewModel/GoogleHistoryViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace ExcelWvvm.ViewModel
 {
@@ -11,11 +13,23 @@ namespace ExcelWvvm.ViewModel
         public string SecurityId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public RelayCommand<Window> CloseCommand { get; set; }
+
         public GoogleHistoryViewModel()
         {
             this.SecurityId = "";
             this.StartDate = DateTime.Now;
             this.EndDate = this.StartDate.AddDays(7);
+            this.CloseCommand = new RelayCommand<Window>(CloseWindow);
+        }
+
+        public void CloseWindow(Window win)
+        {
+            if (win != null)
+            {
+                WindowHelper.CloseWindow(win);
+            }
         }
     }
 }
